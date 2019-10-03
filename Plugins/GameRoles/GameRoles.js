fc63ffe96c43a0b5150800eef1c5c7d275283d2b
wagyourtail <ruwagray@gmail.com>
2019-10-03 21:22:23 +0000

    fixed GameRoles

--- Plugins/GameRoles/GameRoles.js
+++ Plugins/GameRoles/GameRoles.js
@@ -11,7 +11,7 @@ class addgame extends Discord.Command {
         if (!roles[guild.id]) {
             roles[guild.id] = {roles:{}, message:null};
         }
-        let matches = content.match(/[^\d]*(\d+)[^\d]+?(\d+)/);
+        let matches = content.match(/[^\d]*(\d+)[^\d]+?:[^\s]+:[^\d]*(\d+)/);
         if (matches && matches[1]) {
             roles[guild.id].roles[matches[2]] = matches[1];
         }
@@ -39,37 +39,38 @@ class gamerolelist extends Discord.Command {
     constructor() {
         super ("gamerolelist", ["gamerolelist","grl"], "gamerolelist [optional message id]", "post message for people to react to to get roles", "GameRoles", false);
     }
-    addReactions(message) {
+    addReactions(message, guild) {
         Object.keys(roles[guild.id].roles).forEach(reaction => {
             message.react(reaction);
         });
         message.reactions.forEach(reaction => {
-            if (!Object.keys(roles[guild.id].roles).includes(reaction)) {
-                message.reactions.get(reaction).fetchUsers().then(users => {
+            if (!Object.keys(roles[guild.id].roles).includes(reaction._emoji.id)) {
+                reaction.fetchUsers().then(users => {
                     users.forEach(user => {
-                        message.reactions.get(reaction).remove(user);
+                        reaction.remove(user);
                     });
                 });
             }
         });
-        roles[guild.id].message = {channel:messages.last().channel.id, id:messages.last().id};
+        roles[guild.id].message = {channel:message.channel.id, id:message.id};
         Discord.util.writeJSONSync("Plugins/GameRoles/GameRoles.json", roles);
     }
     message(content, author, channel, guild, message, handler) {
         if (!roles[guild.id]) {
             roles[guild.id] = {roles:{}, message:null};
         }
-        message.delete();
         let matches = content.match(/[^\d]*(\d+)/);
         if (matches && matches[1]) {
             channel.fetchMessage(matches[1]).then(message => {
-                this.addReactions(message);
+                this.addReactions(message, guild);
             });
         } else {
             channel.fetchMessages({limit:2}).then(messages => {
-                this.addReactions(messages.last());
+                this.addReactions(messages.last(), guild);
             });
         }
+
+        message.delete();
     }
 }
 
@@ -81,10 +82,10 @@ module.exports.load = function (client) {
     }
 
     client.on('raw', event => {
-        if (event.t == "MESSAGE_REACTION_ADD" && roles[event.d.guild_id] && roles[event.d.guild_id].message && roles[event.d.guild_id].message.id == event.d.message_id && roles[event.d.guild_id].roles[event.d.emoji.id]) {
-                client.guilds.get(event.d.guild_id).members.get(event.d.user_id).addRole(roles[event.d.guild_id].roles[event.d.emoji.id]);
-        } else if (event.t == "MESSAGE_REACTION_REMOVE" && roles[event.d.guild_id] && roles[event.d.guild_id].message && roles[event.d.guild_id].message.id == event.d.message_id && roles[event.d.guild_id].roles[event.d.emoji.id]) {
-                client.guilds.get(event.d.guild_id).members.get(event.d.user_id).removeRole(roles[event.d.guild_id].roles[event.d.emoji.id]);
+        if (event.t == "MESSAGE_REACTION_ADD" && roles[event.d.guild_id] && roles[event.d.guild_id].message && roles[event.d.guild_id].message.id == event.d.message_id && roles[event.d.guild_id].roles[event.d.emoji.id] && event.d.user_id != client.user.id) {
+                client.guilds.get(event.d.guild_id).members.get(event.d.user_id).addRole(roles[event.d.guild_id].roles[event.d.emoji.id]).catch(console.log);
+        } else if (event.t == "MESSAGE_REACTION_REMOVE" && roles[event.d.guild_id] && roles[event.d.guild_id].message && roles[event.d.guild_id].message.id == event.d.message_id && roles[event.d.guild_id].roles[event.d.emoji.id] && event.d.user_id != client.user.id) {
+                client.guilds.get(event.d.guild_id).members.get(event.d.user_id).removeRole(roles[event.d.guild_id].roles[event.d.emoji.id]).catch(console.log);
         }
     });
 
